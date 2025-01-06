c15e3e76872b3cad9cba59392c3f708af498c153
Juan Rojas <yo217@hotmail.es>
2025-01-06 16:38:08 -0500

    feat: :sparkles: menu x

--- src/app/components/NavPrincipal.tsx
+++ src/app/components/NavPrincipal.tsx
@@ -1,8 +1,12 @@
 "use client";
 import Link from "next/link";
-import { useState } from "react";
+import { Dispatch, useEffect, useState } from "react";
 
-const NavLinks = () => {
+interface INavLinks {
+	setIsOpen: Dispatch<any>;
+}
+
+const NavLinks = ({ setIsOpen }: INavLinks) => {
 	const Styles = {
 		list: `
       w-full
@@ -41,6 +45,7 @@ const NavLinks = () => {
 				<Link
 					href="/"
 					className={Styles.listLink}
+					onClick={() => setIsOpen(false)}
 				>
 					Inicio
 				</Link>
@@ -49,6 +54,7 @@ const NavLinks = () => {
 				<Link
 					href="/gallery"
 					className={Styles.listLink}
+					onClick={() => setIsOpen(false)}
 				>
 					Galeria
 				</Link>
@@ -57,6 +63,7 @@ const NavLinks = () => {
 				<Link
 					href="/contact"
 					className={Styles.listLink}
+					onClick={() => setIsOpen(false)}
 				>
 					Contacto
 				</Link>
@@ -66,31 +73,56 @@ const NavLinks = () => {
 };
 
 const NavPrincipal = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  }
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+	const toggleMenu = () => {
+		setIsOpen(!isOpen);
+	};
 
 	return (
 		<div className="absolute sm:relative h-fit z-10 w-full">
 			<div className="select-none h-[100px] hidden sm:flex bg-gray-600">
-				<NavLinks />
+				<NavLinks setIsOpen={setIsOpen} />
 			</div>
 			<div className="sm:hidden fixed w-full bg-gray-600 flex flex-col">
-        <div className="flex justify-between items-center w-full h-[72px] px-4">
-          <button onClick={toggleMenu}>X</button>
-          <Link
-            href={"/"}
-            className="italic text-lg"
-          >
-            Tuti-Cars
-          </Link>
-        </div>
-        {isOpen && (
-          <div className="">
-            <NavLinks />
-          </div>
-        )}
+				<div className="flex justify-between items-center w-full h-[72px] px-4">
+					<button
+						className="text-2xl"
+						onClick={toggleMenu}
+					>
+						<div
+							className={`absolute transition-all transform duration-500 ease-out ${
+								isOpen ? "rotate-[45deg] -translate-y-4" : "rotate-90 -translate-y-6"
+							}`}
+						>
+							&#124;
+						</div>
+						<div
+							className={`absolute transition-all transform duration-500 ease-out ${
+								isOpen ? "rotate-[135deg] -translate-y-[.9rem]" : "rotate-90 -translate-y-4"
+							}`}
+						>
+							&#124;
+						</div>
+						<div
+							className={`absolute transition-all transform duration-500 ease-out ${
+								isOpen ? "rotate-[45deg] -translate-y-4" : "rotate-90 -translate-y-2"
+							}`}
+						>
+							&#124;
+						</div>
+					</button>
+					<Link
+						href={"/"}
+						className="italic text-2xl"
+					>
+						Tuti-Cars
+					</Link>
+				</div>
+				{isOpen && (
+					<div className="">
+						<NavLinks setIsOpen={setIsOpen} />
+					</div>
+				)}
 			</div>
 		</div>
 	);
